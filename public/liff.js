1992226da155206dfbb94ec12ad192ddc214cd13
Daniel Gimeno <daniel.gimeno@playnext-lab.co.jp>
2020-04-07 16:45:34 +0900

    add more liff v2 functionality to buttons

--- public/liff.js
+++ public/liff.js
@@ -60,13 +60,30 @@ function registerButtonHandlers() {
     document.getElementById('getLanguageButton').addEventListener('click', function() {
         document.getElementById('displayLanguage').textContent = liff.getLanguage();
     });
-    document.getElementById('getVersionButton').addEventListener('click', function(){
+    document.getElementById('getVersionButton').addEventListener('click', function() {
         document.getElementById('displayVersion').textContent = liff.getVersion();
     });
-    document.getElementById('isInClientButton').addEventListener('click', function(){
+    document.getElementById('isInClientButton').addEventListener('click', function() {
         document.getElementById('displayIsInClient').textContent = liff.isInClient();
     });
-    document.getElementById('isLoggedInButton').addEventListener('click', function(){
+    document.getElementById('isLoggedInButton').addEventListener('click', function() {
         document.getElementById('displayIsLoggedIn').textContent = liff.isLoggedIn();
+    });
+    document.getElementById('isApiAvailableButton').addEventListener('click', function() {
+        if (liff.isApiAvailable('shareTargetPicker')){
+            liff.shareTargetPicker([
+                {
+                    type: "text",
+                    text: "Hello, World!"
+                }
+            ])
+                .then(
+                    alert('ShareTargetPicker was launched')
+                ).catch(function(res) {
+                    alert('Failed to launch ShareTargetPicker')
+                })
+        } else {
+            document.getElementById('displayIsApiAvailable').textContent = 'No';
+        }
     });
 }
