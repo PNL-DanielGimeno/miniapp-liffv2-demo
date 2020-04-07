4995385bb1f7cec17bdd43ffe1f71e03c35041ec
Daniel Gimeno <daniel.gimeno@playnext-lab.co.jp>
2020-04-07 14:25:29 +0900

    add js func and first button

---
+++ public/liff.js
@@ -0,0 +1,57 @@
+window.onload = function() {
+    const useNodeJS = true;
+    const defaultLiffId = "";
+
+    let myLiffId = "";
+
+    if (useNodeJS) {
+        fetch('/send-id')
+            .then(function(reqResponse) {
+                return reqResponse.json();
+            })
+            .then(function(jsonResponse) {
+                myLiffId = jsonResponse.id;
+                initializeLiffOrDie(myLiffId);
+            })
+            .catch(function(error) {
+                document.getElementById('liffAppContent').classList.add('hidden');
+                document.getElementById('nodeLiffIdErrorMessage').classList.remove('hidden');
+            });
+    } else {
+        myLiffId = defaultLiffId;
+        initializeLiffOrDie(myLiffId);
+    }
+}
+
+function initializeLiffOrDie(myLiffId) {
+    if (!myLiffId) {
+        document.getElementById('liffAppContent').classList.add('hidden');
+        document.getElementById('liffIdErrorMessage').classList.remove('hidden');
+    } else {
+        initializeLiff(myLiffId);
+    }
+}
+
+function initializeLiff(myLiffId) {
+    liff
+        .init({
+            liffId: myLiffId
+        })
+        .then(() => {
+            initializeApp();
+        })
+        .catch((err) => {
+            document.getElementById('liffAppContent').classList.add('hidden');
+            document.getElementById('liffInitErrorMessage').classList.remove('hidden');
+        });
+}
+
+function initializeApp() {
+    registerButtonHandlers();
+}
+
+function registerButtonHandlers() {
+    document.getElementById('getOSButton').addEventListener('click', function() {
+        document.getElementById('displayOSDiv').textContent = liff.getOS();
+    });
+}
